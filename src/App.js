e589ecd4acf42c77655e8cfacd76b18ea527c734
Kerstin Wiessmann <kwiessmann@gmail.com>
2025-09-24 20:10:26 +0200

    Added the search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       Hello 
-      </header>
+      <div className="container">
+        <header className="App-header">Hello</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Kerstin Wiessmann</small>
+        </footer>
+      </div>
     </div>
   );
 }
